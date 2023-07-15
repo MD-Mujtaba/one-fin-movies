9d68cd55e5780b4a208b4b503041fcd86dde598e
Mujtaba <mdmujtaba954@gmail.com>
2023-07-15 14:56:27 +0530

    Prod Build in Docs Folder

---
+++ docs/assets/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    environmentName: 'Local',
+
+    baseUrls: {
+      LOGIN_URL: 'https://demo.credy.in/api/v1/usermodule/login/',
+      MOVIES_URL: 'https://demo.credy.in/api/v1/maya/movies/'
+    }
+  };
+  
